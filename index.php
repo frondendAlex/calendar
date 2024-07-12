8536e6e5d248074773e7262161374f427df85ba6
frondendAlex <alex-moskow@list.ru>
2024-07-12 11:40:06 +0300

    Календарь

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="css/style.css">
-    <script scr="js/main.js" defer></script>
+    <script src="js/main.js" defer></script>
 
     <title>Календарь</title>
 
@@ -16,7 +16,63 @@
 
     
 
-    
-    
+    <div class="container">
+
+        <div class="wrapper">
+
+            <div class="sidebar">
+                <p>Тут будет sidebar</p>
+            </div>
+
+
+
+            <div class="calendar" id="calendar">
+
+                <div class="calendar__inner">
+                    <div class="calendar__top-week">
+                        <div class="calendar__top-week-item">
+                            <span class="calendar__top-week-num">12</span>
+                            <span class="calendar__top-week-text">Пт</span>
+                        </div>
+
+                        <div class="calendar__top-week-item">
+                            <span class="calendar__top-week-num">13</span>
+                            <span class="calendar__top-week-text">Сб</span>
+                        </div>
+
+                        <div class="calendar__top-week-item">
+                            <span class="calendar__top-week-num">13</span>
+                            <span class="calendar__top-week-text">Вс</span>
+                        </div>
+
+                        <div class="calendar__top-week-item">
+                            <span class="calendar__top-week-num">14</span>
+                            <span class="calendar__top-week-text">Пн</span>
+                        </div>
+
+                        <div class="calendar__top-week-item">
+                            <span class="calendar__top-week-num">15</span>
+                            <span class="calendar__top-week-text">Вт</span>
+                        </div>
+
+                        <div class="calendar__top-week-item">
+                            <span class="calendar__top-week-num">16</span>
+                            <span class="calendar__top-week-text">Ср</span>
+                        </div>
+
+                        <div class="calendar__top-week-item">
+                            <span class="calendar__top-week-num">17</span>
+                            <span class="calendar__top-week-text">Чт</span>
+                        </div>
+                    </div>
+                </div>
+
+                </div>
+        </div>
+
+        
+
+    </div>
+
 </body>
 </html>
--- js/main.js
+++ js/main.js
@@ -1,6 +1,73 @@
 document.addEventListener('DOMContentLoaded', () => {
     'use strict'
 
+    const calendarEL = document.getElementById('calendar');
+
+    const week = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
+    const month = [
+        'Январь',
+        'Февраль',
+        'Март',
+        'Апрель',
+        'Май',
+        'Июнь',
+        'Июль',
+        'Август',
+        'Сентбярь',
+        'Октябрь',
+        'Ноябрь',
+        'Декабрь'
+    ];
     
+    const now = new Date();
+    const dateWeek = now.getDay() - 1;
+    const dateDay = now.getDate();
+    const dateMonth = now.getMonth();
+    const dateYear = now.getFullYear();
+
+    
+    
+    function w(mount, year) {
+        let daysInMonth = new Date(year, mount, 0).getDate();
+        let a = [];
+
+        for (let i = 0; i <= daysInMonth; i++) {
+            a.push(i);
+        }
+
+        return a;
+
+    }
+
+    const q = w(dateMonth, dateYear);
+
+    console.log(q);
+    
+
+
+
+    // function getAllDaysInMonth(year, month) {
+    //     let daysInMonth = new Date(year, month, 0).getDate();
+    //     console.log(daysInMonth);
+    //     let daysArray = [];
+        
+    //     for (let i = 1; i <= daysInMonth; day++) {
+    //         daysArray.push(i);
+    //     }
+        
+    //     return daysArray;
+    // }
+    
+    // let year = 2021;
+    // let mont = 8; // Номер месяца (январь - 1, февраль - 2, и т.д.)
+    // let daysOfMonth = getAllDaysInMonth(year, mont);
+    // console.log(daysOfMonth);
+
+
+
+
+
+
+
 
 });
