7e34e914092b38cdda0d91ad7fc18e9db6b018a4
frondendAlex <alex-moskow@list.ru>
2024-07-12 08:53:50 +0300

    Календарь

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php require_once('php/req.php') ?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="css/style.css">
+    <script scr="js/main.js" defer></script>
+
+    <title>Календарь</title>
+
+</head>
+<body>
+
+    
+
+    
+    
+</body>
+</html>
